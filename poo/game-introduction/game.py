19843d3e7811e24903dee7709a1db8def6eadd77
unknown <dchiodigoncalves@gmail.com>
2026-06-02 11:53:27 -0300

    feat: frog moving

--- poo/game-introduction/game.py
+++ poo/game-introduction/game.py
@@ -1,11 +1,8 @@
 import arcade
 from pyglet.math import Vec2
 
-WIDTH = 800
-HEIGHT = 600
-TITLE = "Meu Jogo"
-
-SCENE_SPEED = 100.0
+SCREEN_WIDTH, SCREEN_HEIGHT = arcade.get_display_size()
+SCENE_SPEED = 50.0
 
 class Entity(arcade.Sprite):
     def __init__(self, filename, scale, speed: float = 0.0):
@@ -31,19 +28,34 @@ class Entity(arcade.Sprite):
             self.change_y * delta_time
         )
 
-class Frog(Entity):
-    def __init__(self):
-        super().__init__("frog.png", 0.2, 250.0)
+class EntScene(Entity):
+    def __init__(self, filename, scale, speed: float = 0.0):
+        super().__init__(filename, scale, speed)
+    
+    def scene_move(self, delta_time):
+        self.center_y -= SCENE_SPEED * delta_time
 
-class Player(Entity):
+    def update(self, delta_time):
+        super().update(delta_time)
+        self.scene_move(delta_time)
+
+class Frog(EntScene):
+    def __init__(self):
+        super().__init__("frog.png", 0.2, 200.0)
+    
+    def set_direction(self):
+        self.direction = Vec2(1.0, 0.0)
+
+class Player(Entity):           
 
     def __init__(self):
-        super().__init__("player.png", 1.5, 250.0)
+        super().__init__("player.png", 1.5, 300.0)
 
         self.move_left : bool = False
         self.move_right : bool = False
         self.move_up : bool = False
         self.move_down : bool = False
+        
 
     def handle_key_press(self, key):
         if key == arcade.key.A:
@@ -72,24 +84,23 @@ class Player(Entity):
             self.move_down = False
     
     def set_direction(self):
-        self.direction = Vec2(self.move_right - self.move_left, self.move_up - self.move_down)
-
+        self.direction = Vec2((self.move_right - self.move_left) * 1.4, self.move_up - self.move_down)
 
 class Game(arcade.Window):
 
     def __init__(self):
-        super().__init__(WIDTH, HEIGHT, TITLE)
+        super().__init__(fullscreen=True, title="Frog streak")
 
         arcade.set_background_color(arcade.color.GRAY)
 
         self.obj_list = arcade.SpriteList()
 
         self.player = Player()
-        self.player.position = (400.0, 100.0)
+        self.player.position = (SCREEN_WIDTH / 2, 100.0)
 
-        for i in range(5):
+        for i in range(4):
             frog = Frog()
-            frog.position = (100.0 + i * 150.0, 500.0)
+            frog.position = ((SCREEN_HEIGHT / 2 + i * 300.0), SCREEN_HEIGHT - 100)
             self.obj_list.append(frog)
 
         self.obj_list.append(self.player)
@@ -109,13 +120,11 @@ class Game(arcade.Window):
         self.player.handle_key_press(key)
 
     def on_key_release(self, key, modifiers):
-        self.player.handle_key_release(key) 
+        self.player.handle_key_release(key)         
 
-        
-
-def executar():
+def execute():
     window = Game()
     arcade.run()
 
 if __name__ == "__main__":
-    executar()
+    execute()
