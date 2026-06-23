42af3371e0999e77b6d613b5e8c635aaf8b8b1be
unknown <dchiodigoncalves@gmail.com>
2026-06-23 09:24:37 -0300

    enemys add and room control

--- poo/game-introduction/game.py
+++ poo/game-introduction/game.py
@@ -1,8 +1,23 @@
 import arcade
+import random
+
 from pyglet.math import Vec2
 
 SCREEN_WIDTH, SCREEN_HEIGHT = arcade.get_display_size()
-SCENE_SPEED = 50.0
+SCENE_SPEED = 100.0
+
+
+
+frog_sound = arcade.load_sound("frog_sound.mp3")
+
+class TelaInicial(arcade.View):
+    def __init__(self):
+        super().__init__()
+    
+    def on_draw(self):
+        self.clear()
+        arcade.draw_text("Pressione enter para jogar", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, arcade.color.GREEN, 20)
+        return super().on_draw()
 
 class Entity(arcade.Sprite):
     def __init__(self, filename, scale, speed: float = 0.0):
@@ -11,22 +26,47 @@ class Entity(arcade.Sprite):
         self.speed = speed
 
     def set_direction(self):
-        self.direction = Vec2(0.0, 0.0)
+        pass
 
     def update(self, delta_time):
         self.set_direction()
 
         self.change_x = self.speed * self.direction.x
+        self.change_y = self.speed * self.direction.y
 
         self.center_x += (
             self.change_x * delta_time
         )
-        
-        self.change_y = self.speed * self.direction.y
 
         self.center_y += (
             self.change_y * delta_time
         )
+
+        self.check_exit_x()
+        self.check_exit_y()
+
+    def check_exit_x(self):
+        if (self.right > SCREEN_WIDTH):
+            self.change_x = 0
+            self.right = SCREEN_WIDTH
+            self.on_exit_window()
+        elif (self.left < 0):
+            self.change_x = 0
+            self.left = 0
+            self.on_exit_window()
+    
+    def check_exit_y(self):
+        if (self.top > SCREEN_HEIGHT):
+            self.change_y = 0
+            self.top = SCREEN_HEIGHT
+            self.on_exit_window()
+        elif (self.bottom < 0):
+            self.change_y = 0
+            self.bottom = 0
+            self.on_exit_window()
+
+    def on_exit_window(self):
+        pass
 
 class EntScene(Entity):
     def __init__(self, filename, scale, speed: float = 0.0):
@@ -38,13 +78,38 @@ class EntScene(Entity):
     def update(self, delta_time):
         super().update(delta_time)
         self.scene_move(delta_time)
-
+    
+    def check_exit_y(self):
+        if (self.bottom < 0):
+            self.change_y = 0
+            self.bottom = 0
+            self.on_exit_window()
+    
 class Frog(EntScene):
-    def __init__(self):
-        super().__init__("frog.png", 0.2, 200.0)
+    def __init__(self, speed : float = 0.0):
+        super().__init__("frog.png", 0.2, speed)
+        self.exit_window = False
+    
+    def on_exit_window(self):        
+        self.remove_from_sprite_lists()
+        return super().on_exit_window()
+
+    def set_direction(self):
+        pass
+
+class Enemy(EntScene):
+    def __init__(self, scale, speed : float = 0.0):
+        super().__init__("player.png", scale, speed)
+    
+    def update(self, delta_time):
+        super().update(delta_time)
     
     def set_direction(self):
-        self.direction = Vec2(1.0, 0.0)
+        self.direction = Vec2(0, -1.0)
+
+    def check_exit_y(self):
+        if (self.top < 0):
+            self.remove_from_sprite_lists()
 
 class Player(Entity):           
 
@@ -69,6 +134,8 @@ class Player(Entity):
         
         if key == arcade.key.S:
             self.move_down = True
+        
+
 
     def handle_key_release(self, key):
         if key == arcade.key.A:
@@ -86,10 +153,9 @@ class Player(Entity):
     def set_direction(self):
         self.direction = Vec2((self.move_right - self.move_left) * 1.4, self.move_up - self.move_down)
 
-class Game(arcade.Window):
-
+class GameWorld(arcade.View):
     def __init__(self):
-        super().__init__(fullscreen=True, title="Frog streak")
+        super().__init__()
 
         arcade.set_background_color(arcade.color.GRAY)
 
@@ -97,33 +163,89 @@ class Game(arcade.Window):
 
         self.player = Player()
         self.player.position = (SCREEN_WIDTH / 2, 100.0)
-
-        for i in range(4):
-            frog = Frog()
-            frog.position = ((SCREEN_HEIGHT / 2 + i * 300.0), SCREEN_HEIGHT - 100)
-            self.obj_list.append(frog)
-
         self.obj_list.append(self.player)
+
+    def on_update(self, delta_time):
+        self.obj_list.update(delta_time)         
+
+        self.spawn_frogs()
+        self.spawn_enemy()
+
+        for obj in self.obj_list:
+            if isinstance(obj, Frog) and arcade.check_for_collision(self.player, obj):
+                self.obj_list.remove(obj)
+                arcade.play_sound(frog_sound)
+        for obj in self.obj_list:
+            if isinstance(obj, Enemy) and arcade.check_for_collision(self.player, obj):
+                self.player.remove_from_sprite_lists()
 
     def on_draw(self):
         self.clear()
         self.obj_list.draw()
     
-    def on_update(self, delta_time):
-        self.obj_list.update(delta_time)
-
-        for obj in self.obj_list:
-            if isinstance(obj, Frog) and arcade.check_for_collision(self.player, obj):
-                self.obj_list.remove(obj)
+    def spawn_frogs(self):
+        if (random.randint(1, 90) == 1):
+            if (random.randint(1, 2) == 1):
+                frog = Frog(random.randint(40 , 70))
+                frog.position = ((SCREEN_HEIGHT / 2 - 200), SCREEN_HEIGHT)
+                self.obj_list.append(frog)
+                frog.angle = 90
+                frog.direction = Vec2(1, 0)
+            else:
+                frog = Frog(random.randint(40 , 70))
+                frog.position = ((SCREEN_HEIGHT / 2 + 800), SCREEN_HEIGHT)
+                self.obj_list.append(frog)
+                frog.angle = 270
+                frog.direction = Vec2(-1, 0)
+    
+    def spawn_enemy(self):
+        if (random.randint(1, 90) == 1):
+            rua = random.randint(1,4)
+            enemy_speed = enemy_speed = random.randint(50,100)
+            enemy_angle = 0
+            if (rua > 2):
+                enemy_angle = 180
+                enemy_speed *= 2
+            enemy = Enemy(2, enemy_speed)
+            enemy.position =  ((SCREEN_HEIGHT / 2 + (rua * 220.0) - 100), SCREEN_HEIGHT)
+            enemy.angle = enemy_angle
+            self.obj_list.append(enemy)
     
     def on_key_press(self, key, modifiers):
         self.player.handle_key_press(key)
-
+        if key == arcade.key.ESCAPE:
+            start_window = StartWindow()
+            self.window.show_view(start_window)
+    
     def on_key_release(self, key, modifiers):
-        self.player.handle_key_release(key)         
+        self.player.handle_key_release(key)
+    
+class StartWindow(arcade.View):
+    def __init__(self, window = None, background_color = arcade.color.GRAY):
+        super().__init__(window, background_color)
+    
+    def on_draw(self):
+        self.clear()
+        
+        start_txt = arcade.Text(
+            "Pressione [Enter] para jogar",
+            SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
+        )
 
+        start_txt.draw()
+    
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.J or key == arcade.key.ENTER:
+            game_world = GameWorld()
+            self.window.show_view(game_world)
+    
 def execute():
-    window = Game()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, title="Frog streak")
+    
+    start_window = StartWindow()
+
+    window.show_view(start_window)
+    
     arcade.run()
 
 if __name__ == "__main__":
